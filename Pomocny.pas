ea08393f02a7892c45f80933e7017604992a8b01
Ján Hrnčírik <jan.hrncirik@gmail.com>
2023-12-21 15:02:56 +0100

    Prispôsobenie binárneho vyhľadávania, hľadaniu fixu otvorenia odletu

--- Pomocny.pas
+++ Pomocny.pas
@@ -1,40 +1,53 @@
-// binary searches Begin
+//Pôvodné vyhľadávania fixu otborenia odletu
+if NbrFixes > 0 then
         begin
-          item := Trunc(Pilots[i].start);
+          while  (j < NbrFixes) and (Pilots[i].Fixes[j].TSec < Task.NoStartBeforeTime) do 
+          begin
+            j := J + 1;
+          end;
+//
+//binárne vyhľadávanie fixu času otvorenia odletu 
+  center, Vleft, Vright, Vresult, item: Integer;
+// binary searches Begin
+        if Task.NoStartBeforeTime <= 0 then // Nie je nastavený čas otvorenia odletu!
+          begin
+            Info1 := 'Nie je nastavený čas otvorenia odletu! Nastavte čas otvorenia odletu!!!' ;
+            exit;
+          end;
+        begin
+          item := Task.NoStartBeforeTime;
           Vleft:=0;
           Vright:= GetArrayLength(Pilots[i].Fixes) - 1;
           if Vright < 0 then
             begin
-               Info1 := 'Vright = -1';
+               Info1 := 'Vright = -1, IGC súbor je prázdny. Má 0 fixov.';
                Exit;
             end;
-          if ((item < Pilots[i].Fixes[Vleft].Tsec) or (item > Pilots[i].Fixes[Vright].Tsec)) then // element out of scope
+          if ((item < Pilots[i].Fixes[Vleft].Tsec) or (item > Pilots[i].Fixes[Vright].Tsec)) then // element out of scope, Čas otvorenia odletu je mimo rozsah fixov IGC súboru.
           begin
-            Vresult:=-1;
-            Info1 := 'element out of scope item = ' + IntToStr(item);
+            Vresult:=-1; //príznak pre ladenie, -1 pilotov odlet je mimo fixov, 1 fix nájdený, 2 fix nájdený – interval fixov väčší ako 1 s
+            Info1 := 'element out of scope, Čas otvorenia odletu je mimo rozsah fixov IGC súboru. item = ' + GetTimeString(item);
             exit;
           end;
 
-          while (Vleft <= Vright) and (Vright > 20) do begin // if we have something to share
+          while (Vleft <= Vright) and (Vright > 20) do begin // if we have something to share, Ak máme čo deliť
             center:=(Vleft + Vright) div 2;
             if (item = Pilots[i].Fixes[center].Tsec) then
               begin
-                PilotStartAlt := Pilots[i].Fixes[center].AltQnh;
-                PilotStartSpeed := Pilots[i].Fixes[center].Gsp;
-                Vresult:= 1; // found
-                Break; // Ending the loop while
+                j := center;
+                Vresult:= 1; // found, nájdené, príznak pre ladenie
+                Break; // Ending the loop while, Ukonči slučku while!
             end
             else
             if (item < Pilots[i].Fixes[center].Tsec) then
-              Vright:=center - 1 // throw away the Vright half
+              Vright:=center - 1 // throw away the Vright half, zahodiť pravú (Vright) polovicu
             else
-              Vleft:=center + 1; // discard the Vleft half
-              if (item < Pilots[i].Fixes[Vleft].Tsec) then
+              Vleft:=center + 1; // discard the Vleft half, zahodiť ľavú (Vleft) polovicu
+              if (item < Pilots[i].Fixes[Vleft].Tsec) then //nebol 1 sekundový záznam, priradí najbližší vyšší fix po čase otvorenia odletu
                 begin
-                  PilotStartAlt := (Pilots[i].Fixes[center].AltQnh + Pilots[i].Fixes[center+1].AltQnh) / 2;
-                  PilotStartSpeed := (Pilots[i].Fixes[center].Gsp + Pilots[i].Fixes[center+1].Gsp) / 2;
-                  Vresult:= 2; // found
-                  Break; // Ending the loop while
+                  j := center + 1;
+                  Vresult:= 2; // found, nájdené, príznak pre ladenie
+                  Break; // Ending the loop while, Ukonči slučku while!
                 end;
 
             end;
